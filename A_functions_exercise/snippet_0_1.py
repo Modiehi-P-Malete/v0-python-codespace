69a740a50e78e61e63417361e8aa1214316dca40
Patience M <172664646+Modiehi-P-Malete@users.noreply.github.com>
2026-01-13 13:57:32 +0000

    Add snippets for function exercises in A_functions_exercise

---
+++ A_functions_exercise/snippet_0_1.py
@@ -0,0 +1,14 @@
+def greet():
+    print("hey")
+    print("programmers")
+
+def whistle():
+    print("doot")
+
+print("first")
+print("second")
+greet()
+print("third")
+print("fourth")
+whistle()
+
---
+++ A_functions_exercise/snippet_0_2.py
@@ -0,0 +1,15 @@
+def how_many():
+    return 42
+
+print(how_many)
+print(how_many())
+
+the_answer = how_many()
+print(the_answer)
+
+def how_much():
+    5   # does nothing
+
+print(how_much())
+
+
