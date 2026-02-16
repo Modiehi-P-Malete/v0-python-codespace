8ccc5f3603cd03badc4184a525a183d372951368
Patience M <modiehipmalete@gmail.com>
2026-02-16 16:45:31 +0200

    Implement `pick_perfect_squares` function to filter perfect squares from a list

---
+++ C_decomposing_problems_exercise/pick_perfect_squares.py
@@ -0,0 +1,13 @@
+# Write a function `pick_perfect_squares` that accepts a list of numbers.
+# The function should return a list containing only the perfect squares.
+#
+# A perfect square is a number that can be written as n * n.
+
+print(pick_perfect_squares([6,4,81,21,36]))
+# [4, 81, 36]
+
+print(pick_perfect_squares([100,24,144]))
+# [100, 144]
+
+print(pick_perfect_squares([30,25]))
+# [25]
