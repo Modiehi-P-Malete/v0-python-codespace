bd6d2bf2d3e7f2597bc4af28c0ddbdcf4463abeb
Patience M <modiehipmalete@gmail.com>
2026-02-17 14:16:19 +0200

    Example of creating and writing to a file named 'output.txt' using the "w"/ write() method

---
+++ File_Management/output_write.py
@@ -0,0 +1,8 @@
+file =open("File_Management\output.txt","w")
+file.write("Hello Python\n")
+file.write("File handling is useful")
+file.close()
+
+#The output will be a file named output.txt with the following content:
+#Hello Python
+#File handling is useful
