0799a8b3671f16cca8d172e83372bc3a7d02796a
Patience M <modiehipmalete@gmail.com>
2026-02-17 14:20:24 +0200

    Example of appending/adding a new line into output.txt

---
+++ File_Management/output_append.py
@@ -0,0 +1,8 @@
+file =open("File_Management\output.txt","a")
+file.write("\nLearning step by step")
+file.close()
+
+#The output will be a file named output.txt with the following content:
+#Hello Python   
+#File handling is useful
+#Learning step by step
